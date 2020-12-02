1d64f8b1c64afdce97a1cb2a95f4bb683246b836
Minsung Kim <minsungkim@Minsungui-MacBookPro.local>
2020-12-02 15:43:13 +0900

    fix some errors

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Route exact path="/" component={Main} />
         <Route path="/about" component={Search} />
       </div>
     );
--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -5,7 +5,6 @@ const Main = () => {
     return (
         <div>
             <ul>
-                <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
             </ul>
             <hr />
